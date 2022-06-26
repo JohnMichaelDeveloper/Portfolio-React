0a32829510b1609895df18252df5d938ca4883c5
JohnMichaelDeveloper <johnfrontend@gmail.com>
2022-06-26 16:07:42 -0300

    Adicionando pasta animatedLetters, indexJs e indexScss

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assests/img/jm-vertical.png'
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = [' ','J', 'o', 'h', 'n', ' ', 'M', 'i', 'c','h', 'a','e','l']
+  const jobArray = ['W', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
   return (
     <div className="container home-page">    
    
       <div className="text-zone">
-        <h1>Hi, <br /> I'm
-        <img src={LogoTitle} alt="developer" />
-        lobodan
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+        <AnimatedLetters letterClass={letterClass}  strArray={nameArray} idx={15} />
+        <img src={LogoTitle} className="invert" alt="developer" />
+        
         <br/>
-        web developer
+        <AnimatedLetters letterClass={letterClass}  strArray={jobArray} idx={22} />
+       
         </h1>
         <h2>Frontend Developer / JavaScript / YouTuber</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
